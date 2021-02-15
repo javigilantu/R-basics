5aa4113f767eb46d2306fb9fb52c264f531bcf1b
javigilantu <javigilantu@gmailcom>
2021-02-15 11:34:36 +0100

    Ejercicio day 1 R instakado tidyverse

---
+++ ej_day_1.R
@@ -0,0 +1,14 @@
+# para el ejercicio de hoy copia las siguientes líneas en tu script
+# RECUERDA instalar el paquete tidyverse
+install.packages("tidyverse")
+library(tidyverse)
+# cargamos los datos
+data("mtcars")
+head(mtcars)
+
+# limpio los datos
+datos_limpios <- mtcars %>%
+  filter(cyl == 4)
+
+# grafico los datos
+plot(x = mtcars$wt, y = mtcars$mpg)
